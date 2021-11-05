4dd10f8adbcdb6a9e24f92314cb1bc428efef9c1
mecher_ <bradenchasefranklin@icloud.com>
2021-11-05 10:00:03 -0700

    sine functions and number pad

--- calcTest.py
+++ calcTest.py
@@ -1,19 +1,20 @@
-import math
+from math import sin
+
 
 def evalInput(equationIn, turtleWindow, color):
         turtleWindow.color(color)
         turtleWindow.penup()
         turtleWindow.goto(0, 0)
         turtleWindow.forward(2)
-        equationOut1 = equationIn.replace("x", str(1))
+        equationOut1 = equationIn.replace("x", str(-100))
         equationOut1 = eval(equationOut1)
         turtleWindow.goto(10, equationOut1 * 10)
         turtleWindow.pendown()
-        for x in range(-9, 10):
+        for x in range(-99, 100):
             equationOut = equationIn.replace("x", str(x))
             equationOut = eval(equationOut)
             print("old", str(x*10))
-            print("scaled",str(x) )
+            print("scaled",str(x))
             turtleWindow.goto(x *10, equationOut * 10)
 
 
--- createWidgets.py
+++ createWidgets.py
@@ -8,18 +8,30 @@ def insertIntoTextBox(textBox, insertVal):
     textBox.insert(END, str(insertVal))
     textBox["state"] = DISABLED
 
+def clearTextBox(textBox):
+    textBox["state"] = NORMAL
+    textBox.delete('1.0', END)
+    textBox["state"] = DISABLED
+
 def createCalcButtons(app, textBox):
+    # numpad
     bInt1 = tkinter.Button(app, text = str(1), width = 2, command = lambda: insertIntoTextBox(textBox, 1)).place(x = 600, y = 375)
     bInt2 = tkinter.Button(app, text = str(2), width = 2, command = lambda: insertIntoTextBox(textBox, 2)).place(x = 625, y = 375)
     bInt3 = tkinter.Button(app, text = str(3), width = 2, command = lambda: insertIntoTextBox(textBox, 3)).place(x = 650, y = 375)
     bInt4 = tkinter.Button(app, text = str(4), width = 2, command = lambda: insertIntoTextBox(textBox, 4)).place(x = 600, y = 350)
     bInt5 = tkinter.Button(app, text = str(5), width = 2, command = lambda: insertIntoTextBox(textBox, 5)).place(x = 625, y = 350)
     bInt6 = tkinter.Button(app, text = str(6), width = 2, command = lambda: insertIntoTextBox(textBox, 6)).place(x = 650, y = 350)
-    bInt7 = tkinter.Button(app, text = str(7), width = 2, command = lambda: insertIntoTextBox(textBox, 7))
-    bInt8 = tkinter.Button(app, text = str(8), width = 2, command = lambda: insertIntoTextBox(textBox, 8))
-    bInt9 = tkinter.Button(app, text = str(9), width = 2, command = lambda: insertIntoTextBox(textBox, 9))
+    bInt7 = tkinter.Button(app, text = str(7), width = 2, command = lambda: insertIntoTextBox(textBox, 7)).place(x = 600, y = 325)
+    bInt8 = tkinter.Button(app, text = str(8), width = 2, command = lambda: insertIntoTextBox(textBox, 8)).place(x = 625, y = 325)
+    bInt9 = tkinter.Button(app, text = str(9), width = 2, command = lambda: insertIntoTextBox(textBox, 9)).place(x = 650, y = 325)
     bInt0 = tkinter.Button(app, text = str(0), width = 2, command = lambda: insertIntoTextBox(textBox, 0)).place(x = 625, y = 400)
 
+    # action buttons 
+    bActEq = tkinter.Button(app, text = "=", width = 3, command = lambda: insertIntoTextBox(textBox, "=")).place(x = 675, y = 350)
+    bActX1 = tkinter.Button(app, text = "*x", width = 3, command = lambda: insertIntoTextBox(textBox, "*x")).place(x = 675, y = 375)
+    bActX2 = tkinter.Button(app, text = "x", width = 3, command = lambda: insertIntoTextBox(textBox, "x")).place(x = 715, y = 375)
+    bActSin = tkinter.Button(app, text = "sin(", width = 3, command = lambda: insertIntoTextBox(textBox, "sin(")).place(x = 675, y = 400)
+    bActClosePara = tkinter.Button(app, text = ")", width = 3, command = lambda: insertIntoTextBox(textBox, ")")).place(x = 715, y = 400)
 def openFile():
     file = filedialog.askopenfile(filetypes =[('Python Graphing Calculator Files', '*.pygraph')])
     content = file.read()
@@ -29,10 +41,10 @@ def openFile():
 
 def createTxtBtns(app, turtle):
     sbx = 501
-    text = tkinter.Text(app, width = 20, state = DISABLED)
-    text.place(x = sbx, y = 100)
-    clr = tkinter.Button(app, height = 2, width = 3, text = "CLR").place(x = sbx + 150, y = 100)
-    ref = tkinter.Button(app, height = 2, width = 10, text = "Refresh/Draw", command = lambda: evalInput(text.get(), turtleWindow = turtle, color = "blue")).place(x = sbx + 180, y = 100)
+    text = tkinter.Text(app, width = 12, height = 2, state = DISABLED)
+    text.place(x = sbx + 3, y = 100)
+    clr = tkinter.Button(app, height = 2, width = 3, text = "CLR", command = lambda: clearTextBox(text)).place(x = sbx + 150, y = 100)
+    ref = tkinter.Button(app, height = 2, width = 10, text = "Refresh/Draw", command = lambda: evalInput(text.get("1.0","end"), turtleWindow = turtle, color = "blue")).place(x = sbx + 180, y = 100)
     createCalcButtons(app, text)
 
 def createMenuBar(app, saveFunction, newFunction, openFunction):
